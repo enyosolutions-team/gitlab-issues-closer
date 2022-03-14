06dcb8e5a8d0acbc8925abc7beddbb2adf9c75c2
lbandrade <lucas.b@enyosolutions.com>
2022-03-14 13:47:07 -0300

    refactor(cleanOldIssues): add try catch block

--- src/services/gitlab/cleanOldIssues.js
+++ src/services/gitlab/cleanOldIssues.js
@@ -11,9 +11,14 @@ const headers = {
 
 module.exports = async () => {
   const issues = await gitlab.getIssues(issuesUrl, headers, thresholdDate);
+
   for (const issue of issues) {
-    const body = { state_event: 'close' }
-    await gitlab.postCommentOnIssue(issuesUrl, headers, issue.iid, config.message)
-    await gitlab.updateIssue(issuesUrl, headers, issue.iid, body)
+    try {
+      const body = { state_event: 'close' }
+      await gitlab.postCommentOnIssue(issuesUrl, headers, issue.iid, config.message)
+      await gitlab.updateIssue(issuesUrl, headers, issue.iid, body)
+    } catch (eror) {
+      console.error(`Error closing issue ${issue.iid}`)
+    }
   }
 }
