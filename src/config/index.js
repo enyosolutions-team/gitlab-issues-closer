0c32724d116011d2a8ca44191fd59d244f251ee5
lbandrade <lucas.b@enyosolutions.com>
2022-03-14 12:01:34 -0300

    feat: add params read from env

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config({ path: `${__dirname}/.env` })
+
+module.exports = {
+  apiUrl: process.env.API_URL,
+  threshold: parseInt(process.env.THRESHOLD),
+  personalToken: process.env.PERSONAL_TOKEN,
+  projectId: parseInt(process.env.PROJECT_ID)
+}
