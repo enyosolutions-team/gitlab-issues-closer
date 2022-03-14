779d5241a9d1e9624a3c40b1d13eddf396470230
lbertoncello <lucas_b_andrade@hotmail.com>
2022-03-14 13:27:39 -0300

    feat: add service to clean on issues

---
+++ src/services/gitlab/cleanOldIssues.js
@@ -0,0 +1,19 @@
+const gitlab = require('../../resources/gitlab')
+const config = require('../../config')
+
+const thresholdDate = new Date();
+thresholdDate.setDate(thresholdDate.getDate() - config.threshold)
+const baseUrl = `${config.apiUrl}/${config.projectId}`;
+const issuesUrl = `${baseUrl}/issues`;
+const headers = {
+  Authorization: `Bearer ${config.personalToken}`
+};
+
+module.exports = async () => {
+  const issues = await gitlab.getIssues(issuesUrl, headers, thresholdDate);
+  for (const issue of issues) {
+    const body = { state_event: 'close' }
+    await gitlab.postCommentOnIssue(issuesUrl, headers, issue.iid, config.message)
+    await gitlab.updateIssue(issuesUrl, headers, issue.iid, body)
+  }
+}
