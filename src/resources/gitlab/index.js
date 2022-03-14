caead8811b9665f58a76d7b6db15f06621a6034d
lbandrade <lucas.b@enyosolutions.com>
2022-03-14 14:03:25 -0300

    refactor(resources): add try catch in gitlab resources

--- src/resources/gitlab/index.js
+++ src/resources/gitlab/index.js
@@ -2,31 +2,43 @@ const axios = require('axios');
 
 module.exports = {
   getIssues: async (url, headers, thresholdDate) => {
-    const res = await axios({
-      method: 'get',
-      url: `${url}?updated_before=${thresholdDate.toISOString()}&state=opened`,
-      headers: headers
-    })
-    return res.data;
+    try {
+      const res = await axios({
+        method: 'get',
+        url: `${url}?updated_before=${thresholdDate.toISOString()}&state=opened`,
+        headers: headers
+      })
+      return res.data;
+    } catch (error) {
+      console.error('Error getting issues', error)
+    }
   },
   updateIssue: async (url, headers, issueIid, body) => {
-    const res = await axios({
-      method: 'put',
-      url: `${url}/${issueIid}`,
-      headers: headers,
-      data: body
-    })
-    return res.data
+    try {
+      const res = await axios({
+        method: 'put',
+        url: `${url}/${issueIid}`,
+        headers: headers,
+        data: body
+      })
+      return res.data
+    } catch (error) {
+      console.error('Error updating issue', error)
+    }
   },
   postCommentOnIssue: async (url, headers, issueIid, message) => {
-    const res = await axios({
-      method: 'post',
-      url: `${url}/${issueIid}/notes`,
-      headers: headers,
-      data: {
-        body: message
-      }
-    })
-    return res.data
+    try {
+      const res = await axios({
+        method: 'post',
+        url: `${url}/${issueIid}/notes`,
+        headers: headers,
+        data: {
+          body: message
+        }
+      })
+      return res.data
+    } catch (error) {
+      console.error('Error posting comment on issue', error)
+    }
   }
 }
